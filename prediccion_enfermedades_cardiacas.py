5f2499d2540b2efddafbab9269665ced757a423b
javiermar20000 <javiermar1200@gmail.com>
2025-06-09 14:37:38 -0400

    cambios owo

--- prediccion_enfermedades_cardiacas.py
+++ prediccion_enfermedades_cardiacas.py
@@ -46,7 +46,7 @@ log_pred = log_best.predict(X_test)                     # Realiza predicciones d
 log_proba = log_best.predict_proba(X_test)[:, 1]        # Calcula las probabilidades predichas de la clase positiva (1) para métricas como ROC AUC
 
 # KNN
-knn_params = {'n_neighbors': list(range(1, 10))}        # Define una grilla de valores de vecinos (de 1 a 30) para probar en el clasificador KNN
+knn_params = {'n_neighbors': list(range(1, 10))}        # Define una grilla de valores de vecinos (de 1 a 9) para probar en el clasificador KNN
 knn_model = GridSearchCV(KNeighborsClassifier(), knn_params, cv=5)  # Crea un GridSearchCV para buscar el mejor número de vecinos usando validación cruzada
 knn_model.fit(X_train, y_train)                         # Entrena el modelo KNN con los mejores hiperparámetros
 knn_best = knn_model.best_estimator_                    # Extrae el mejor modelo encontrado por GridSearch
@@ -54,7 +54,7 @@ knn_pred = knn_best.predict(X_test)                     # Predice las clases del
 knn_proba = knn_best.predict_proba(X_test)[:, 1]        # Calcula las probabilidades predichas de la clase positiva (1)
 
 # Random Forest
-rf_params = {'max_depth': list(range(1, 10))}           # Define una grilla de valores para la profundidad máxima del árbol (1 a 20)
+rf_params = {'max_depth': list(range(1, 10))}           # Define una grilla de valores para la profundidad máxima del árbol (1 a 9)
 rf_model = GridSearchCV(RandomForestClassifier(random_state=42), rf_params, cv=5)  # Crea un GridSearchCV para ajustar el clasificador Random Forest buscando la mejor profundidad
 rf_model.fit(X_train, y_train)                          # Entrena el modelo Random Forest con los mejores hiperparámetros
 rf_best = rf_model.best_estimator_                      # Extrae el mejor modelo Random Forest encontrado
